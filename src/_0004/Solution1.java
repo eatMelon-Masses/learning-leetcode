55d5bb3a376b4f757964b64cbc93accef0cf9731
yiezi <11764404@qq.com>
2020-01-17 17:00:44 +0800

    update: 更新4题解算法

--- src/_0004/Solution1.java
+++ src/_0004/Solution1.java
@@ -7,17 +7,20 @@ package _0004;
 public class Solution1 {
     public double findMedianSortedArrays(final int[] nums1, final int[] nums2) {
         int len = nums1.length + nums2.length;
-
-        return dg(nums1,nums2,0,0,len/2);
+        //奇数和偶数合并求解，
+        int m = (len+1)/2;
+        int n = (len+2)/2;
+        int sum = dg(nums1,nums2,0,0,n) + dg(nums1,nums2,0,0,m);
+        return sum/2;
     }
     public int dg(int[] num1,int[] num2,int start1,int start2,int k){
         int temp =k/2;
-        int last1 = num1.length-start1;
-        int last2 = num2.length-start2;
-        if (last1<=0) return num2[temp];
-        if (last2<=0) return num1[temp];
-        int i = last1<temp?num1[last1]:num1[temp];
-        int j = last2<temp?num2[last2]:num2[temp];
+        int last1 = start1+temp-1;
+        int last2 = start2+temp-1;
+        if (last1 >= num1.length) return num2[num1.length-1];
+        if (last2 >= num2.length) return num1[num2.length-1];
+        int i = num1[last1];
+        int j = num2[last2];
         if (k==1){
             return Math.min(num1[start1], num2[start2]);
         }
@@ -29,16 +32,10 @@ public class Solution1 {
         return dg(num1,num2,start1,start2,k-k/2);
     }
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++) {
             Solution1 solution = new Solution1();
-            int[] arr1 = {1, 2};
+            int[] arr1 = {1, 3};
             int[] arr2 = {1, 2, 3, 4};
             System.out.println(solution.findMedianSortedArrays(arr1,arr2));
-        }
-        long endTime = System.currentTimeMillis();
-
-        System.out.println(endTime-startTime);
 
     }
 }
