150ffdcd9a4964c6b843416c61ed4a0e7fa4fd15
周晔 <11764404@qq.com>
2020-09-10 19:52:28 +0800

    feat: 140未完成，运行结果超时

--- src/_0140/Solution1.java
+++ src/_0140/Solution1.java
@@ -1,24 +1,33 @@
 package _0140;
 
+import java.math.BigInteger;
 import java.util.*;
 
 /**
  * 运行超时
  */
-public class Solution  {
+public class Solution1 {
     public List<String> wordBreak(String s, List<String> wordDict) {
-        return word_Break(s, new HashSet<>(wordDict), 0);
+        HashMap<String, List<String>> memorandum = new HashMap<>();
+        return word_Break(s, new HashSet<>(wordDict), 0, new HashMap<>());
     }
 
-    public List<String> word_Break(String s, Set<String> wordDict, int start) {
+    public List<String> word_Break(String s, Set<String> wordDict, int start, Map<String, List<String>> memorandum) {
+
         ArrayList<String> suffix = new ArrayList<>();
-        if (start == s.length()){
+        if (start == s.length()) {
             suffix.add("");
         }
         for (int end = start + 1; end <= s.length(); end++) {
             String temp = s.substring(start, end);
             if (wordDict.contains(temp)) {
-                List<String> list = word_Break(s, wordDict, end);
+                List<String> list;
+                String key = s.substring(end);
+                if (memorandum.containsKey(key)) {
+                    list = memorandum.get(key);
+                }else {
+                    list = word_Break(s, wordDict, end, memorandum);
+                }
                 for (String e :
                         list) {
                     suffix.add(s.substring(start, end) + ("".equals(e) ? "" : " ") + e);
@@ -31,8 +40,10 @@ public class Solution  {
     }
 
     public static void main(String[] args) {
+        BigInteger[] dp = new BigInteger[1];
         Solution solution = new Solution();
         List<String> results = solution.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", Arrays.asList("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"));
+
         System.out.println(results);
     }
 }
