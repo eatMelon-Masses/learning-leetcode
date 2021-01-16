f2290aabb13402afff1eeef5549480e7f25dea5a
yiezi <11764404@qq.com>
2021-01-16 23:06:12 +0800

    feat: 1202

--- src/_1202/Solution.java
+++ src/_1202/Solution.java
@@ -18,14 +18,78 @@ package _1202;
 
  */
 
-import java.util.List;
+
+
+import java.util.*;
 
 public class Solution {
-    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+    int[] fa;
+    int[] rank;
+    HashMap<Integer, PriorityQueue<Character>> map = new HashMap<>();
 
+    //初始化秩 元素序列
+    public void init() {
+        for (int i = 0; i < fa.length; i++) {
+            fa[i] = i;
+            rank[i] = 1;
+        }
+    }
+    //查找根节点，压缩路径
+    public int find(int x) {
+        return x == fa[x] ? x : (fa[x] = find(fa[x]));
 
     }
+    //合并子数
+    public void merge(int i, int j) {
+        int x = find(i);
+        int y = find(j);
+        if (rank[x] <= rank[y]) {
+            fa[x] = fa[y];
+        }else {
+            fa[y] = fa[x];
+        }
+        if ((rank[x] == rank[y]) && x!=y) {
+            rank[x]++;
+        }
+    }
 
+    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+        fa = new int[s.length()];
+        rank = new int[s.length()];
+        init();
+        //建立查并集
+        for (int i = 0; i < pairs.size(); i++) {
+            int x = pairs.get(i).get(0);
+            int y = pairs.get(i).get(1);
+            merge(x, y);
+        }
+        //根节点为key value为优先队列 对同一个根节点里所有元素排序
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int fa = find(i);
+            PriorityQueue<Character> p = map.getOrDefault(fa, new PriorityQueue<>());
+            p.add(c);
+            map.put(fa, p);
+        }
+        char[] result = new char[s.length()];
+        //输出
+        for (int i = 0; i < s.length(); i++) {
+            PriorityQueue<Character> p = map.get(fa[i]);
 
+            result[i] = p.poll();
+        }
+        return new String(result);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String string = "dcab";
+        ArrayList<List<Integer>> lists = new ArrayList<>();
+        lists.add(Arrays.asList(0, 3));
+        lists.add(Arrays.asList(1, 2));
+        lists.add(Arrays.asList(0, 2));
+        String s = solution.smallestStringWithSwaps(string, lists);
+        System.out.println(s);
+    }
 
 }
